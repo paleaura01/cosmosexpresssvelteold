63ec6af7fe03d36420a42bd1138e903396fd3077
Aaron Deas <deas.aaron@gmail.com>
2023-02-14 21:36:59 -0500

    Fixed overflow issues on sidenav

--- src/dashboard/sidenavigation/sidenav.svelte
+++ src/dashboard/sidenavigation/sidenav.svelte
@@ -11,7 +11,7 @@
 		close: `hidden`,
 		container: `pb-32 lg:pb-6`,
 		open: `absolute w-8/12 z-40 sm:w-5/12`,
-		default: `bg-white shadow h-screen overflow-y-auto top-0 lg:block lg:relative lg:w-64 lg:z-auto`
+		default: `bg-white shadow h-screen top-0 lg:block lg:relative lg:w-64 lg:z-auto`
 	};
 
 </script>
