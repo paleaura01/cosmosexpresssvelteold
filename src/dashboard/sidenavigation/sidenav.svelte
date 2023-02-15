414878c610f01401313b052fc7d4e3237edabfb4
Aaron Deas <deas.aaron@gmail.com>
2023-02-14 20:13:21 -0500

    Fixed home page

--- src/dashboard/sidenavigation/sidenav.svelte
+++ src/dashboard/sidenavigation/sidenav.svelte
@@ -1,20 +1,7 @@
 <script>
-
-	import SidenavItems from './sidenavitems.svelte';
-	import SidenavHeader from './sidenavheader.svelte';
-
-	const style = {
-		mobilePosition: {
-			left: 'left-0',
-			right: 'right-0'
-		},
-		close: `hidden`,
-		container: `pb-32 lg:pb-6`,
-		open: `absolute w-8/12 z-40 sm:w-5/12`,
-		default: `bg-white shadow h-screen overflow-y-auto top-0 lg:block lg:relative lg:w-64 lg:z-auto`
-	};
-
-
+  import SidenavItems from './sidenavitems.svelte';
+  import SidenavHeader from './sidenavheader.svelte';
+import { open} from '../provider/store.js'
   function tglDM() {
     isDm = !isDm;
   }
