8dc04db834ca514d58ec1c15da0832f46f53f697
paleaura01 <pale.aura01@gmail.com>
2023-02-14 20:47:11 -0500

    1
    1

--- src/dashboard/sidenavigation/sidenavheader.svelte
+++ src/dashboard/sidenavigation/sidenavheader.svelte
@@ -1,3 +1,3 @@
-<div class="bg-white dark:bg-gray-800 flex items-center justify-center mb-6 pb-6 sticky top-0 z-10">
-	<img src="/images/1.jpg" width={120} height={120} alt="Lucha y vence" />
+<div class="bg-white dark:bg-gray-800 flex items-center justify-center mt-6 pb-6 sticky top-0 z-10">
+	<img src="/images/Cosmos_express_logo.svg" width={190} height={190} alt="Lucha y vence" />
 </div>
