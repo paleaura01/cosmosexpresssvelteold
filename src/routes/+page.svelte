b7a5ba018c9ddec3b6cce5c788173ca06ca45a0b
Aaron Deas <deas.aaron@gmail.com>
2023-02-14 19:57:39 -0500

    Added dark mode to context

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
       >
         <div class="w-11/12 flex h-full text-white flex-col">
           <div
-            class="h-full flex flex-col items-center w-full justify-evenly border"
+            class="h-full flex flex-col items-center w-full justify-between border"
           >
             <div class="dark:text-white w-1/3 flex justify-between w-full">
               <div class="bg-red-500 h-16 w-1/6" />
@@ -38,6 +38,11 @@
               <div class="bg-yellow-900 h-16 flex-grow" />
               <div class="bg-pink-200 h-16 flex-grow" />
               <div class="bg-orange-400 h-16 flex-grow" />
+            </div>
+            <div class="dark:text-white w-1/3 flex">
+              <div class="bg-yellow-500 h-16 flex-grow" />
+              <div class="bg-pink-500 h-16 flex-grow" />
+              <div class="bg-gray-500 h-16 flex-grow" />
             </div>
             <div class="dark:text-white w-1/3 flex">
               <div class="bg-yellow-500 h-16 flex-grow" />
